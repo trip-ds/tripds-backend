3f3288225484a0f8d9d90756dba6eaae45aae933
jijongkwon <help3451@naver.com>
2024-05-16 16:13:21 +0900

    Feat(MemberService) : 유저 정보 조회 메서드 선언

--- src/main/java/com/ssafy/tripds/member/model/service/MemberService.java
+++ src/main/java/com/ssafy/tripds/member/model/service/MemberService.java
@@ -5,7 +5,8 @@ import com.ssafy.tripds.member.model.dto.MemberDto;
 
 public interface MemberService {
     MemberDto login(MemberDto memberDto) throws Exception;
-    void saveRefreshToken(String userId, String refreshToken) throws Exception;
-    Object getRefreshToken(String userId) throws Exception;
+    MemberDto userInfo(String email) throws Exception;
+    void saveRefreshToken(String email, String refreshToken) throws Exception;
+    Object getRefreshToken(String email) throws Exception;
     void deleRefreshToken(String userId) throws Exception;
 }
