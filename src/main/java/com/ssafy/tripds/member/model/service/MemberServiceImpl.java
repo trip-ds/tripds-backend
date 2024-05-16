212083a6f7e5cc0f28a10989e0f8cd2e0179a05e
jijongkwon <help3451@naver.com>
2024-05-16 13:08:01 +0900

    Feat(MemberServiceImpl) : Token 저장, 얻기, 삭제 메서드 구현

--- src/main/java/com/ssafy/tripds/member/model/service/MemberServiceImpl.java
+++ src/main/java/com/ssafy/tripds/member/model/service/MemberServiceImpl.java
@@ -5,6 +5,9 @@ import com.ssafy.tripds.member.model.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService{
@@ -14,5 +17,26 @@ public class MemberServiceImpl implements MemberService{
     @Override
     public MemberDto login(MemberDto memberDto) throws Exception {
         return memberMapper.login(memberDto);
+    }
+
+    @Override
+    public void saveRefreshToken(String userId, String refreshToken) throws Exception {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("userId", userId);
+        map.put("token", refreshToken);
+        memberMapper.saveRefreshToken(map);
+    }
+
+    @Override
+    public Object getRefreshToken(String userId) throws Exception {
+        return memberMapper.getRefreshToken(userId);
+    }
+
+    @Override
+    public void deleRefreshToken(String userId) throws Exception {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("userId", userId);
+        map.put("token", null);
+        memberMapper.deleteRefreshToken(map);
     }
 }
