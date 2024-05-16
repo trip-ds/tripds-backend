183c9654a104e2d1ea97f87e01d6f593564d99cc
jijongkwon <help3451@naver.com>
2024-05-16 13:08:07 +0900

    Feat(MemberServiceImpl) : Token 저장, 얻기, 삭제 메서드 구현

--- src/main/java/com/ssafy/tripds/member/model/mapper/MemberMapper.java
+++ src/main/java/com/ssafy/tripds/member/model/mapper/MemberMapper.java
@@ -4,8 +4,12 @@ import com.ssafy.tripds.member.model.dto.MemberDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.sql.SQLException;
+import java.util.Map;
 
 @Mapper
 public interface MemberMapper {
     MemberDto login(MemberDto memberDto) throws SQLException;
+    void saveRefreshToken(Map<String, String> map) throws SQLException;
+    Object getRefreshToken(String userid) throws SQLException;
+    void deleteRefreshToken(Map<String, String> map) throws SQLException;
 }
--- src/main/java/com/ssafy/tripds/member/model/service/MemberService.java
+++ src/main/java/com/ssafy/tripds/member/model/service/MemberService.java
@@ -5,4 +5,7 @@ import com.ssafy.tripds.member.model.dto.MemberDto;
 
 public interface MemberService {
     MemberDto login(MemberDto memberDto) throws Exception;
+    void saveRefreshToken(String userId, String refreshToken) throws Exception;
+    Object getRefreshToken(String userId) throws Exception;
+    void deleRefreshToken(String userId) throws Exception;
 }
