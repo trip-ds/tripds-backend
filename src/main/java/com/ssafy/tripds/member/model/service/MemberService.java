49f5ebf82c2c8f535d25915e6f9c26e047a2602b
jijongkwon <help3451@naver.com>
2024-05-16 17:28:27 +0900

    Feat(MemberService) : 회원가입 메서드 선언

--- src/main/java/com/ssafy/tripds/member/model/service/MemberService.java
+++ src/main/java/com/ssafy/tripds/member/model/service/MemberService.java
@@ -3,7 +3,10 @@ package com.ssafy.tripds.member.model.service;
 
 import com.ssafy.tripds.member.model.dto.MemberDto;
 
+import java.sql.SQLException;
+
 public interface MemberService {
+    int signup(MemberDto memberDto) throws Exception;
     MemberDto login(MemberDto memberDto) throws Exception;
     MemberDto userInfo(String email) throws Exception;
     void saveRefreshToken(String email, String refreshToken) throws Exception;
