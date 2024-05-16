f51b6bc9f82cce6140db9d31ce2bf8a127bb5672
jijongkwon <help3451@naver.com>
2024-05-16 10:27:09 +0900

    init project

---
+++ src/main/java/com/ssafy/tripds/TripdsApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.tripds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/tripds/TripdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tripds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
