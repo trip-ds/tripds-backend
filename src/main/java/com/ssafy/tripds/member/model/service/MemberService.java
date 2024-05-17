bac7485e1f73f899c827573a3271742c7428ab9d
jijongkwon <help3451@naver.com>
2024-05-17 10:36:45 +0900

    Feat(MemberService) : 유저 정보 수정 메서드 선언

--- src/main/java/com/ssafy/tripds/member/model/service/MemberService.java
+++ src/main/java/com/ssafy/tripds/member/model/service/MemberService.java
@@ -9,6 +9,7 @@ public interface MemberService {
     int signup(MemberDto memberDto) throws Exception;
     MemberDto login(MemberDto memberDto) throws Exception;
     MemberDto userInfo(String email) throws Exception;
+    int updateMember(MemberDto memberDto) throws Exception;
     void saveRefreshToken(String email, String refreshToken) throws Exception;
     Object getRefreshToken(String email) throws Exception;
     void deleRefreshToken(String userId) throws Exception;
