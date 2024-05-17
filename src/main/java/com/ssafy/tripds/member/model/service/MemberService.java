bf6260354b032b02fab7b102ec0f9fda0d7aa824
jijongkwon <help3451@naver.com>
2024-05-17 15:13:42 +0900

    refactor(MemberService) : 비즈니스 로직 분리를 위한 타입 변경

--- src/main/java/com/ssafy/tripds/member/model/service/MemberService.java
+++ src/main/java/com/ssafy/tripds/member/model/service/MemberService.java
@@ -4,12 +4,13 @@ package com.ssafy.tripds.member.model.service;
 import com.ssafy.tripds.member.model.dto.MemberDto;
 
 import java.sql.SQLException;
+import java.util.Map;
 
 public interface MemberService {
-    int signup(MemberDto memberDto) throws Exception;
-    MemberDto login(MemberDto memberDto) throws Exception;
-    MemberDto userInfo(String email) throws Exception;
-    int updateMember(MemberDto memberDto) throws Exception;
+    int signup(MemberDto memberDto);
+    Map<String, Object> login(MemberDto memberDto);
+    MemberDto userInfo(String email);
+    int updateMember(MemberDto memberDto);
     void saveRefreshToken(String email, String refreshToken) throws Exception;
     Object getRefreshToken(String email) throws Exception;
     void deleRefreshToken(String userId) throws Exception;
