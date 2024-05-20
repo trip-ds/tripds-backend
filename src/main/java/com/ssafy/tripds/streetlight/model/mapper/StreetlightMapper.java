eadfdfd4e1a5e7c264552a4ada22a1e2b7313775
jijongkwon <help3451@naver.com>
2024-05-20 22:52:00 +0900

    Feat(StreetlightMapper) : 근처 가로등 얻는 메서드 선언

--- src/main/java/com/ssafy/tripds/streetlight/model/mapper/StreetlightMapper.java
+++ src/main/java/com/ssafy/tripds/streetlight/model/mapper/StreetlightMapper.java
@@ -1,4 +1,13 @@
 package com.ssafy.tripds.streetlight.model.mapper;
 
+import com.ssafy.tripds.streetlight.model.dto.CoordinateDto;
+import com.ssafy.tripds.streetlight.model.dto.StreetlightDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Mapper
 public interface StreetlightMapper {
+    List<StreetlightDto> getNearbyStreetLights(CoordinateDto coordinateDto) throws SQLException;
 }
