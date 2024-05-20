c38de958c6936e51559697fb784018e17f276895
jijongkwon <help3451@naver.com>
2024-05-21 00:19:06 +0900

    Feat(CctvService) : 근처 cctv 가져오는 메서드 선언

--- src/main/java/com/ssafy/tripds/cctv/model/service/CctvService.java
+++ src/main/java/com/ssafy/tripds/cctv/model/service/CctvService.java
@@ -1,4 +1,9 @@
 package com.ssafy.tripds.cctv.model.service;
 
+import com.ssafy.tripds.cctv.model.dto.CctvDto;
+import com.ssafy.tripds.cctv.model.dto.CoordinateDto;
+import java.util.List;
+
 public interface CctvService {
+    List<CctvDto> getNearbyCctv(CoordinateDto coordinateDto);
 }
