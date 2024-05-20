2afc761aa89227859c4f5ea3a132deb1d4649aff
jijongkwon <help3451@naver.com>
2024-05-20 22:52:05 +0900

    Feat(StreetlightService) : 근처 가로등 얻는 메서드 선언

--- src/main/java/com/ssafy/tripds/streetlight/model/service/StreetlightService.java
+++ src/main/java/com/ssafy/tripds/streetlight/model/service/StreetlightService.java
@@ -1,4 +1,12 @@
 package com.ssafy.tripds.streetlight.model.service;
 
+import com.ssafy.tripds.streetlight.model.dto.CoordinateDto;
+import com.ssafy.tripds.streetlight.model.dto.StreetlightDto;
+
+import java.util.List;
+
 public interface StreetlightService {
+
+    List<StreetlightDto> getNearbyStreetLights(CoordinateDto coordinateDto);
+
 }
