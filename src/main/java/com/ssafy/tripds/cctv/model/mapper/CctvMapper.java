5c265868142d8e5cc68278028a537f822fee6d55
jijongkwon <help3451@naver.com>
2024-05-21 10:40:48 +0900

    Feat(CctvMapper) : 동서남북 위경도 이용한 cctv 불러오기 메서드 선언

--- src/main/java/com/ssafy/tripds/cctv/model/mapper/CctvMapper.java
+++ src/main/java/com/ssafy/tripds/cctv/model/mapper/CctvMapper.java
@@ -1,6 +1,7 @@
 package com.ssafy.tripds.cctv.model.mapper;
 
 import com.ssafy.tripds.cctv.model.dto.CctvDto;
+import com.ssafy.tripds.cctv.model.dto.CctvSearchDto;
 import com.ssafy.tripds.cctv.model.dto.CoordinateDto;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -10,4 +11,5 @@ import java.util.List;
 @Mapper
 public interface CctvMapper {
     List<CctvDto> getNearbyCctv(CoordinateDto coordinateDto) throws SQLException;
+    List<CctvDto> getCctvFromMap(CctvSearchDto cctvSearchDto) throws SQLException;
 }
