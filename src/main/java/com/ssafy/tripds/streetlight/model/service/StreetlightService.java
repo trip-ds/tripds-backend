f11137f383c45d4a4292aafafb28ad41c834632c
jijongkwon <help3451@naver.com>
2024-05-21 13:41:23 +0900

    Feat(StreetlightService) : 동서남북 위경도 이용한 가로등 정보 불러오기 메서드 선언

--- src/main/java/com/ssafy/tripds/streetlight/model/service/StreetlightService.java
+++ src/main/java/com/ssafy/tripds/streetlight/model/service/StreetlightService.java
@@ -2,11 +2,10 @@ package com.ssafy.tripds.streetlight.model.service;
 
 import com.ssafy.tripds.streetlight.model.dto.CoordinateDto;
 import com.ssafy.tripds.streetlight.model.dto.StreetlightDto;
-
+import com.ssafy.tripds.streetlight.model.dto.StreetlightSearchDto;
 import java.util.List;
 
 public interface StreetlightService {
-
     List<StreetlightDto> getNearbyStreetLights(CoordinateDto coordinateDto);
-
+    List<StreetlightDto> getStreetlightFromMap(StreetlightSearchDto streetlightSearchDto);
 }
