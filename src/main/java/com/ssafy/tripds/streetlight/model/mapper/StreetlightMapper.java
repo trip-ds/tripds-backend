77b78dc734888d7c1f0b14c35104003a2bc78cb9
jijongkwon <help3451@naver.com>
2024-05-21 13:41:15 +0900

    Feat(StreetlightMapper) : 동서남북 위경도 이용한 가로등 정보 불러오기 메서드 선언

--- src/main/java/com/ssafy/tripds/streetlight/model/mapper/StreetlightMapper.java
+++ src/main/java/com/ssafy/tripds/streetlight/model/mapper/StreetlightMapper.java
@@ -2,6 +2,7 @@ package com.ssafy.tripds.streetlight.model.mapper;
 
 import com.ssafy.tripds.streetlight.model.dto.CoordinateDto;
 import com.ssafy.tripds.streetlight.model.dto.StreetlightDto;
+import com.ssafy.tripds.streetlight.model.dto.StreetlightSearchDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.sql.SQLException;
@@ -10,4 +11,5 @@ import java.util.List;
 @Mapper
 public interface StreetlightMapper {
     List<StreetlightDto> getNearbyStreetLights(CoordinateDto coordinateDto) throws SQLException;
+    List<StreetlightDto> getStreetlightFromMap(StreetlightSearchDto streetlightSearchDto) throws  SQLException;
 }
