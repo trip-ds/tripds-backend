3548bb58b43af30e0412cd658788dac89e0b6aa5
jijongkwon <help3451@naver.com>
2024-05-22 11:18:51 +0900

    Feat(EstateMapper) : 관심부동산 조회를 위한 메서드 선언

--- src/main/java/com/ssafy/tripds/estateplanner/model/mapper/EstateMapper.java
+++ src/main/java/com/ssafy/tripds/estateplanner/model/mapper/EstateMapper.java
@@ -1,12 +1,15 @@
 package com.ssafy.tripds.estateplanner.model.mapper;
 
+import com.ssafy.tripds.estateplanner.model.dto.EstateDto;
 import com.ssafy.tripds.estateplanner.model.dto.EstateInterestDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.sql.SQLException;
+import java.util.List;
 
 @Mapper
 public interface EstateMapper {
     int insertEstateInterest(EstateInterestDto estateInterestDto) throws SQLException;
-
+    List<String> selectEstateInterestByMemberId(Long memberId) throws SQLException;
+    List<EstateDto> getEstateInfoByRegisterNumbers(List<String> registerNumberList) throws SQLException;
 }
