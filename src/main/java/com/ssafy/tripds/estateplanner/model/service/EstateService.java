d15ca807f79da20c77164204f492dae7bd23546b
jijongkwon <help3451@naver.com>
2024-05-22 11:18:59 +0900

    Feat(EstateService) : 관심부동산 조회를 위한 메서드 선언

--- src/main/java/com/ssafy/tripds/estateplanner/model/service/EstateService.java
+++ src/main/java/com/ssafy/tripds/estateplanner/model/service/EstateService.java
@@ -1,7 +1,12 @@
 package com.ssafy.tripds.estateplanner.model.service;
 
+import com.ssafy.tripds.estateplanner.model.dto.EstateDto;
 import com.ssafy.tripds.estateplanner.model.dto.EstateInterestDto;
+
+import java.util.List;
 
 public interface EstateService {
     int insertEstateInterest(EstateInterestDto estateInterestDto);
+    List<String> selectEstateInterestByMemberId(Long memberId);
+    List<EstateDto> getEstateInfoByRegisterNumbers(List<String> registerNumberList);
 }
