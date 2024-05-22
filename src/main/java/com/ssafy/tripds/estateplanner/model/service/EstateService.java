097c546f19205331f9ae0c1aa9cc241df42591eb
jijongkwon <help3451@naver.com>
2024-05-22 10:43:42 +0900

    Feat(EstateService) : 관심부동산 등록 메서드 선언

---
+++ src/main/java/com/ssafy/tripds/estateplanner/model/service/EstateService.java
@@ -0,0 +1,7 @@
+package com.ssafy.tripds.estateplanner.model.service;
+
+import com.ssafy.tripds.estateplanner.model.dto.EstateInterestDto;
+
+public interface EstateService {
+    int insertEstateInterest(EstateInterestDto estateInterestDto);
+}
