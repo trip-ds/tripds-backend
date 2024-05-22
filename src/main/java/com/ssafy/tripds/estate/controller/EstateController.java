397552c1abdb68ed5cf4c7567b403b64531b6bfd
jijongkwon <help3451@naver.com>
2024-05-22 14:18:05 +0900

    Feat(EstateController) : 부동산 상세정보 얻는 메서드 컨트롤러 구현

--- src/main/java/com/ssafy/tripds/estate/controller/EstateController.java
+++ src/main/java/com/ssafy/tripds/estate/controller/EstateController.java
@@ -1,10 +1,25 @@
 package com.ssafy.tripds.estate.controller;
 
+import com.ssafy.tripds.estate.model.service.EstateService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/estate")
+@RequiredArgsConstructor
+@Slf4j
 public class EstateController {
 
+    private final EstateService estateService;
+
+    @GetMapping("/detail/{registerNumber}")
+    public ResponseEntity<?> getEstateDetail(@PathVariable("registerNumber") String registerNumber){
+        return new ResponseEntity<>(estateService.getEstateDetail(registerNumber), HttpStatus.OK);
+    }
 }
