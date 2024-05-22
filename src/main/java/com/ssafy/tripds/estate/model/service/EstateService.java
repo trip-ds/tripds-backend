0da45dd51d3fc44c8cb46dee80de88a4ca545350
jijongkwon <help3451@naver.com>
2024-05-22 14:07:26 +0900

    Feat(EstateService) : member_id를 이용해 플래너 정보 불러오는 메서드 선언

--- src/main/java/com/ssafy/tripds/estate/model/service/EstateService.java
+++ src/main/java/com/ssafy/tripds/estate/model/service/EstateService.java
@@ -7,11 +7,14 @@ import com.ssafy.tripds.estate.model.dto.EstatePlannerDto;
 import java.util.List;
 
 public interface EstateService {
-    // 관심 부동산
+    // 부동산 (공통) //
     int insertEstateInterest(EstateInterestDto estateInterestDto);
+
+    // 관심 부동산 //
     List<String> selectEstateInterestByMemberId(Long memberId);
     List<EstateDto> getEstateInfoByRegisterNumbers(List<String> registerNumberList);
 
-    // 부동산 플래너
+    // 부동산 플래너 //
     int insertEstatePlanner(EstatePlannerDto estatePlannerDto);
+    List<String> selectEstatePlannerByMemberId(Long memberId);
 }
