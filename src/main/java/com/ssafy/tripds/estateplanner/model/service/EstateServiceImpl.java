7e3ff0b03beeb3c542020534a20f6cbbcffde807
jijongkwon <help3451@naver.com>
2024-05-22 11:19:08 +0900

    Feat(EstateServiceImpl) : 관심부동산 조회를 위한 메서드 구현

--- src/main/java/com/ssafy/tripds/estateplanner/model/service/EstateServiceImpl.java
+++ src/main/java/com/ssafy/tripds/estateplanner/model/service/EstateServiceImpl.java
@@ -1,11 +1,14 @@
 package com.ssafy.tripds.estateplanner.model.service;
 
+import com.ssafy.tripds.estateplanner.model.dto.EstateDto;
 import com.ssafy.tripds.estateplanner.model.dto.EstateInterestDto;
 import com.ssafy.tripds.estateplanner.model.mapper.EstateMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +22,30 @@ public class EstateServiceImpl implements EstateService {
     public int insertEstateInterest(EstateInterestDto estateInterestDto) {
         try{
             return estateMapper.insertEstateInterest(estateInterestDto);
+        }
+        catch (Exception e){
+            log.error("exception = {}" ,e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional
+    @Override
+    public List<String> selectEstateInterestByMemberId(Long memberId) {
+        try{
+            return estateMapper.selectEstateInterestByMemberId(memberId);
+        }
+        catch (Exception e){
+            log.error("exception = {}" ,e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional
+    @Override
+    public List<EstateDto> getEstateInfoByRegisterNumbers(List<String> registerNumberList) {
+        try{
+            return estateMapper.getEstateInfoByRegisterNumbers(registerNumberList);
         }
         catch (Exception e){
             log.error("exception = {}" ,e);
