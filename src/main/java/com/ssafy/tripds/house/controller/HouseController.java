c0e1cb0e5b0537156f9adc674b8c5c20c16947bf
noah0504789 <noah0504@naver.com>
2024-05-22 12:54:39 +0900

    [refactor/house] add prefix to url

--- src/main/java/com/ssafy/tripds/house/controller/HouseController.java
+++ src/main/java/com/ssafy/tripds/house/controller/HouseController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/house")
+@RequestMapping("/api/house")
 @RequiredArgsConstructor
 public class HouseController {
 
