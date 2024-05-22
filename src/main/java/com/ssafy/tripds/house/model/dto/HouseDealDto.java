a20bd88fa54c16bd0e1f35019f96f6a329e63b3c
noah0504789 <noah0504@naver.com>
2024-05-22 11:35:57 +0900

    [feat/house] house+deal list+deal list summary+deal summary

---
+++ src/main/java/com/ssafy/tripds/house/model/dto/HouseDealDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.tripds.house.model.dto;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("houseDealType")
+public record HouseDealDto(
+//        String roadName,
+        String contractType,
+        String name,
+        Double monthlyRent,
+        Double deposit,
+        Double price,
+        Integer floor,
+        Double netArea
+) {
+}
